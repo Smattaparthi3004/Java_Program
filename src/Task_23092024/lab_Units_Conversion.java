914eaf7a79aa282332fe4b98933de53b51e2fffa
smattaparthi3004 <sowjanyamattaparthi1986@gmail.com>
2024-09-24 10:17:12 +0530

    23rd Exercise files

--- src/Task_23092024/lab_Units_Conversion.java
+++ src/Task_23092024/lab_Units_Conversion.java
@@ -1,31 +1,33 @@
 /* Write a program that converts between different units (e.g., kilometers to miles, Celsius to Fahrenheit)
 based on user selection using a switch statement.
 Input. -
-choice - 1 - km → m, km → 1km
-choice - 2 - f → c, f → c
+choice -1 km -> m
+choice- 2  m -> km
+choice -3  f -> c
+choice -4  c -> f
 */
 package Task_23092024;
 public class lab_Units_Conversion {
     public static void main(String[] args) {
         double value=96.8;
-        String Choice="m";
+        int Choice= 1;
         switch (Choice) {
-            case "Km":
+            case 1:
             {
                 System.out.println(value + " km is equals to :" + (value*1000) +" m");
               break;
             }
-            case "m":
+            case 2:
             {
                 System.out.println(value + " m is equals to :" + (value/1000) +" Km");
                 break;
             }
-            case "f":
+            case 3:
             {
                 System.out.println(value + " F is equals to :" + ((value-32) * 5/9) +" °C");
                break;
             }
-            case "c":
+            case 4:
             {
                 System.out.println(value + "°C is equals to :" + ((value*9/5) + 32) +"F" );
                break;
